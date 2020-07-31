680359c8705ba00b2e71b1a775092f4b834c5638
dragonfist453 <thermosphere453@gmail.com>
2020-07-31 22:01:54 +0530

    Put JS effect in CSS

--- effects.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    i = 0;
-    setInterval(() => {
-        if(i<=100) {
-            document.getElementById("name").style.opacity = i/100;
-            i = i + 1;
-        }
-    },20);
-}
